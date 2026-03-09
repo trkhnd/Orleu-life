74948814898bff454a01a3f4b326df45b3a92210
Dani <danialtorehan8@gmail.com>
2026-03-09 21:18:07 +0500

    some bags

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ app.use(cors())
 app.use(express.json())
 app.use('/api/tasks', taskRoutes)
 
-const PORT = 5000
+const PORT = 5001
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`)
 })
